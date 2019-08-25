dc54ab7338fba89964137232dfb8c430b298e743
Jens N <jens.n@godlike.biz>
2019-08-25 03:28:52 +0200

    Prevent update check when no active window was detected

--- src/gaming-buddy/app.js
+++ src/gaming-buddy/app.js
@@ -443,7 +443,8 @@ class App extends EventEmitter {
     update() {
         this.updateWindows().then(() => {
             // Update plugin
-            if ((this.pluginActive !== null) && this.pluginActive.backend.checkActive(this.windowActive)) {
+            if ((this.pluginActive !== null) && (this.windowActive !== null) &&
+                    this.pluginActive.backend.checkActive(this.windowActive)) {
                 let pluginTarget = this.pluginActive;
                 // Stop update timer
                 this.updateStop();
